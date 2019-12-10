85d2a29260e1b6ae502da46496b09d7eef34e519
Deepak Purushotham <dpurushotham136@gmail.com>
2019-12-10 13:49:40 -0600

    init

---
+++ global.d.ts
@@ -0,0 +1,7 @@
+declare global {
+  interface Window {
+      CANNON:any;
+  }
+}
+
+export {}
---
+++ src/config/water.js
@@ -0,0 +1,7 @@
+export const properties = {
+  windForce: -15,
+  waveHeight: 0,
+  colorBlendFactor: 0.3,
+  bumpHeight: 0.1,
+  waveLength: 0.1
+}
